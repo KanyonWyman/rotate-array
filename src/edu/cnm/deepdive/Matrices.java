b4f719fc3e69ab0ff846b4d110ee8a4db21c732c
kanyon <kanyonwyman@gmail.com>
2019-03-10 20:21:48 -0600

    have code that rotates and in place rotates

--- src/edu/cnm/deepdive/Matrices.java
+++ src/edu/cnm/deepdive/Matrices.java
@@ -1,62 +1,86 @@
 package edu.cnm.deepdive;
 
+import java.util.*;
+
 public class Matrices {
 
-  static void reverseCol(int data[][])
-  {
-    for (int i = 0; i < data[0].length; i++)
-      for (int j = 0, k = data[0].length - 1;
-          j < k; j++, k--) {
-        int temp = data[j][i];
-        data[j][i] = data[k][i];
-        data[k][i] = temp;
+  public static void main(String[] args) {
 
+    int[][] matrix = {
+
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11},
+        {12, 13, 14, 15}};
+
+    int[][] newRotatedMatrix = rotate(matrix);
+
+    for (int i = 0; i < newRotatedMatrix.length;
+        i++) {
+
+      System.out.println("This is 90 rotation row " + i + ":" + Arrays.toString(
+          newRotatedMatrix[i]));
+
+      int[][] matrix2 = {
+
+          {0, 1, 2, 3},
+          {4, 5, 6, 7},
+          {8, 9, 10, 11},
+          {12, 13, 14, 15}};
+      int[][] inPlaceRotate = rotateInPlace(matrix2);
+
+      for (int j = 0; j < inPlaceRotate.length;
+          j++) {
+        System.out.println("In place rotation " + Arrays.toString(inPlaceRotate[j]));
       }
-  }
 
-
-  static void flipRow(int data[][])
-  {
-    for (int i = 0; i < data.length; i++)
-      for (int j = i; j < data[0].length; j++) {
-        int temp2 = data[j][i];
-        data[j][i] = data[i][j];
-        data[i][j] = temp2;
-
-
-      }
-  }
-
-
-  static void printMatrix(int data[][])
-  {
-    for (int i = 0; i < data.length; i++) {
-      for (int j = 0; j < data[0].length; j++)
-        System.out.print(data[i][j] + " ");
-      System.out.println("");
     }
-  }
-
-
-  static void rotate(int data[][])
-  {
-    reverseCol(data);
-    flipRow(data);
 
 
   }
 
 
-  public static void main(String[] args)
-  {
-    int data[][] = { { 1, 2, 3, 4 },
-        { 5, 6, 7, 8 },
-        { 9, 10, 11, 12 },
-        { 13, 14, 15, 16 } };
+  private static int[][] rotate(int[][] data) {
+
+    int[][] rotatedMatrix = new int[data.length][data[0].length];
+
+    for (int row = 0; row < data.length; row++) {
+      for (int col = 0; col < data.length; col++) {
+        System.out
+            .println("This fills the data row [" + row + "] col[" + col + "] = " + data[row][col]);
+      }
+
+    }
+
+    for (int row = 0; row < data.length; row++) {
+      for (int col = 0; col < data[row].length; col++) {
+
+        rotatedMatrix[col][(data.length - 1) - row] = data[row][col];
+
+      }
+
+    }
+
+    return rotatedMatrix;
+
+  }
+  static int N = 4;
+
+  private static int[][] rotateInPlace(int[][] data) {
+    for (int i = 0; i < N / 2; i++)
+    {
+      for (int j = i; j < N - i - 1; j++)
+      {
 
 
-    rotate(data);
-    printMatrix(data);
+        int temp = data[i][j];
+        data[i][j] = data[N - 1 - j][i];
+        data[N - 1 - j][i] = data[N - 1 - i][N - 1 - j];
+        data[N - 1 - i][N - 1 - j] = data[j][N - 1 - i];
+        data[j][N - 1 - i] = temp;
+      }
+    }
+    return data;
   }
 
 
